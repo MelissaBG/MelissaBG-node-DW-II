27d2212a351342a3c07b26ffa7175d1de577bd68
MelissaBG <melissabgoulart@gmail.com>
2022-05-13 21:42:20 -0300

    1 file modified

--- 0.3.3.js
+++ 0.3.3.js
@@ -25,15 +25,18 @@ const resultadoPorGenero = medicos.filter((medicos) => medico.porGenero);
 let arraysDrs = [];
 for (let filme of medicos) {
   if (medico.genero === 'masculino') {
-    const cordial = 'Dr.' + medico.nome + ' ' + medico.sobrenome;
+    const cordial = 'Dr. ' + medico.nome + ' ' + medico.sobrenome;
     arrayDrs.push(cordial);
   }
   if (medico.genero === 'feminino') {
-    const cordial = 'Dra.' + medico.nome + ' ' + medico.sobrenome;
+    const cordial = 'Dra. ' + medico.nome + ' ' + medico.sobrenome;
     arrayDrs.push(cordial);
   }
   if (medico.genero === 'nao-binario') {
-    const cordial = 'Dr(e).' + medico.nome + ' ' + medico.sobrenome;
+    const cordial = 'Dr(e). ' + medico.nome + ' ' + medico.sobrenome;
     arrayDrs.push(cordial);
   }
 }
+//Com Map()
+const novoArrayDrs = medicos.map((medico) => {});
+console.log(arraysDrs);
