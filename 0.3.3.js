96c4940b60d142843d42533ddc17df9616d4345c
MelissaBG <melissabgoulart@gmail.com>
2022-05-13 22:01:56 -0300

    1 file modified

--- 0.3.3.js
+++ 0.3.3.js
@@ -48,8 +48,18 @@ const novoArrayDrs = medicos.map((medico) => {
   }
   return `${medico.nome} ${medico.sobrenome}`;
 });
-function callback(medico, index, array){
-  
+
+//forEach 
+function callback(medico){
+  if (medico.genero === 'masculino') {
+    medico.cordialidade = 'Dr. ' + medico.nome + ' ' + medico.sobrenome;
+  }
+  if (medico.genero === 'feminino') {
+    medico.cordialidade = 'Dra. ' + medico.nome + ' ' + medico.sobrenome;
+  }
+  if (medico.genero === 'nao-binario') {
+    medico.cordialidade = 'Dr(e). ' + medico.nome + ' ' + medico.sobrenome;
+  }
 }
 medicos.forEach(callback);
 console.log(arraysDrs);
