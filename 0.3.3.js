c22175e1f7246186954051a2a4df7648bae6256a
MelissaBG <melissabgoulart@gmail.com>
2022-05-13 21:39:23 -0300

    1 file modified

--- 0.3.3.js
+++ 0.3.3.js
@@ -1,31 +1,39 @@
 const medicos = [
   {
-  nome: 'Leticia',
-  sobrenome: 'Costa',
-  genero: 'feminino',
+    nome: 'Leticia',
+    sobrenome: 'Costa',
+    genero: 'feminino',
   },
   {
-  nome: 'Adimaldo',
-  sobrenome: 'Pinto',
-  genero: 'masculino',
+    nome: 'Adimaldo',
+    sobrenome: 'Pinto',
+    genero: 'masculino',
   },
   {
-  nome: 'Noah',
-  sobrenome: 'Lutus',
-  genero: 'nao-binario',
-  }];
-  /**Faça uma função que irá retornar um array de strings. Essa função deve formatar o
+    nome: 'Noah',
+    sobrenome: 'Lutus',
+    genero: 'nao-binario',
+  },
+];
+/**Faça uma função que irá retornar um array de strings. Essa função deve formatar o
 nome do médico conforme o genero . */
-  const porGenero = (medicos) => medicos.genero
+const porGenero = (medicos) => medicos.genero;
 
-  const resultadoPorGenero = medicos 
-  .filter(medicos => medico.porGenero)
+const resultadoPorGenero = medicos.filter((medicos) => medico.porGenero);
 
-  let arraysDrs = [];
-  for (let filme of medicos){
-    if(medico.genero === 'masculino'){
-    const cordial = medico.nome  + medico.sobrenome;
+// com for
+let arraysDrs = [];
+for (let filme of medicos) {
+  if (medico.genero === 'masculino') {
+    const cordial = 'Dr.' + medico.nome + ' ' + medico.sobrenome;
     arrayDrs.push(cordial);
-    }
-
   }
+  if (medico.genero === 'feminino') {
+    const cordial = 'Dra.' + medico.nome + ' ' + medico.sobrenome;
+    arrayDrs.push(cordial);
+  }
+  if (medico.genero === 'nao-binario') {
+    const cordial = 'Dr(e).' + medico.nome + ' ' + medico.sobrenome;
+    arrayDrs.push(cordial);
+  }
+}
