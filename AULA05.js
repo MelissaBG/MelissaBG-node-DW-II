ce7103adfc16b7116c690b56fbdf336a635c9cc0
MelissaBG <melissabgoulart@gmail.com>
2022-05-27 21:13:10 -0300

    1 file modified

--- AULA05.js
+++ AULA05.js
@@ -1,82 +1,40 @@
-/** sort()
- * Caracteristicas: 
- * Assinatura elemto A e elemento B que variam. Se for 1 vai decrementar o elemento A se for -1 vai decrementar o B.Usar quando quiser ordenar 
- * Macete: 
-*/
-let numeros = [10, 20, 87];
-numeros = numeros.sort((A, B) => {
-  console.log(A);
-  console.log(B);
-  if(A > B) {
-    return -1;
-  }
-  if(A < B) { 
-    return 1;
-  }
-  return 0;
-});
-
-console.log(numeros);
-
-let numeros = [10, 20, 87];
-numeros = numeros.sort((A, B) =>  A - B);
-
-console.log(numeros);
-
-//com objetos
-let numeros = [
-  {
-    nome: 'Mathias',
-    valor: 20,
-    idade: 26
-  },
-  {
-    nome: 'Luiz',
-    valor: 10,
-    idade: 17
-  },
-  {
-    valor: 2,
-  },
-]
-//numeros = numeros.sort((A, B) => {
-  const valorComparacaoA = (A.valor + A.idade)/2;
-  const valorComparacaoB = (B.valor + B.idade)/2;
-  return valorComparacaoB - valorComparacaoA;
-//}
-
-
-//Exemplo macete com objetos
-numeros = numeros.sort((A, B) => B.valor - A.valor);
-console.log(numero);
-
-let novosNumeros = [10, 20, 30]
-//.reduce() Usado para tudo:
-const r = novosNumeros.reduce((acumulador, proximoValor) => acumulador + proximoValor, 0);
-console.log(r); 
-
-//Com objetos
-const aluno ={
-  nome: 'Luiz',
-  presenca:[
-    {
-      nome:'aula01',
-      presente:true,
-    },
-    {
-      nome:'aula02',
-      presente:false,
-    },
-    {
-      nome:'aula03',
-      presente:true,
-    }
-  ]
+//Assicrono
+function buscarInformacoes(){
+  let resultado =[];
+  setTimeout(()=>{ //EXCUÇÃO ASSINCRONA
+    resultado = ['Mathis', 'melissa'];
+    callback(resultado);
+  }, 2000);
+  return resultado;
 }
-const r =aluno.presenca.reduce((acumulador, proximoValor) => {
-  if(proximoValor.presente){
-    return acumulador + 1;
-  }
-  return acumulador;
-}, 0);
-console.log(r); 
+const resultado = buascarInformacoes();
+
+buascarInformacoes((resultado) => {
+  console.log(resultado);
+});
+/** Promises -Promessa de quando finalizar uma excecução vai retorna, gerenciamento de callbacks assincrnos. Diferenciar o que é um callback sincrono de um assincrono. 
+ * Uma promise sempre retorna outra promise. Uma cadeia de promessas.
+*/
+function buscarInformacoes2(){
+  let resultado =[];
+ return new Promise((resolve) => {
+  setTimeout(()=>{ //EXCUÇÃO ASSINCRONA
+    resultado = ['Mathis', 'melissa'];
+    callback(resultado);
+  }, 2000);
+ });
+}
+buscarInformacoes2()
+  .then((resultado) => { //quando minha promessa for resolvida
+    console.log(1, resultado);
+    return buscarInformacoes2(); //Usando return permite usar outro then.
+  })
+  .then((resultado) => {
+    console.log(2, resultado);
+  })
+    .catch(()=> { //falha erro
+
+    }) 
+  .finally(() => {
+    console.log('finally')
+  });
